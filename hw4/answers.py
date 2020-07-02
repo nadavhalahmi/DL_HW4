6fbf006230b3507a444868eecb8c49fc6c66e345
tal <tal.yaakov@campus.technion.ac.il>
2020-07-02 23:50:15 +0300

    added a few answers

--- hw4/answers.py
+++ hw4/answers.py
@@ -49,13 +49,15 @@ def part1_aac_hyperparams():
 part1_q1 = r"""
 **Your answer:**
 
+Subtracting baseline helps reducing the variance if the baseline is defined wisely. For instance, in our case we 
+defined the baseline as the mean of all q-values which is the mean of all estimated state-values. Choosing it that way, 
+helps us normalize the results of the loss based on the current state such that the results of it are closer to 0.
+This makes the model more stable and with less noise.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+An example for a situation where it can help is when there is a safe way to land the spaceship. In this case we would 
+like to prior this way and by using baseline which helps to avoid a sequence of "bad" actions and makes the decision 
+more independent.
+
 
 """
 
@@ -63,13 +65,13 @@ An equation: $e^{i\pi} -1 = 0$
 part1_q2 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+We always get a valid approximation thanks to the connection between the terms $v_\pi(s)$ and $q_\pi(s,a)$. 
+Actually, $v_\pi(s)$ can be expressed as $\sum_{a \in A}\pi(a|s)q_\pi(s,a)$. It means that $v_\pi(s)$ is actually 
+takes into consideration all the possible q-values given state $s$ with respect to the agent behavior. 
+So defining the baseline that way gives us good results since q-values close to this baseline will lead to lower 
+loss values just like we want.
+The value of the loss in this case is actually the lowest when the value estimator is equal to the expected 
+value of the loss of all trajectories from the given state.
 
 """
 
@@ -77,12 +79,14 @@ An equation: $e^{i\pi} -1 = 0$
 part1_q3 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+1. As we can see, the bpg and cpg perforemed the best way,
+and where very stable thanks to the baseline. epg and vpg perforemed
+in a similar way, but were very sensitive, and at some point,
+exploded. when epg converges to 0, we can conclude that we slighly
+over-fitted, and the action is poorly randomized (the entropy
+is low).
+2. AAC got just a bit better results than cpg, during training and "playing"
+with AAC, it felt like it's more stable than cpg and studying at
+a high rate.
 
 """
